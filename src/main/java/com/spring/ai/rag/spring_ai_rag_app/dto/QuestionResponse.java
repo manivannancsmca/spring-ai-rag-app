bc1f725465a18f81e19d057adbbf937bff84dcf0
manivannancsmca <manivannancsmca@gmail.com>
2026-08-18 01:50:06 +0530

    QuestionResponse class added

---
+++ src/main/java/com/spring/ai/rag/spring_ai_rag_app/dto/QuestionResponse.java
@@ -0,0 +1,17 @@
+package com.spring.ai.rag.spring_ai_rag_app.dto;
+
+import java.util.List;
+
+public record QuestionResponse(
+        String question,
+        String answer,
+        int retrievedChunks,
+        List<SourceChunk> sources
+) {
+
+    public record SourceChunk(
+            String content,
+            Object metadata
+    ) {
+    }
+}
