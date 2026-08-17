dbe7261e0246bb10ca4d902582d1dc792840ea30
manivannancsmca <manivannancsmca@gmail.com>
2026-08-18 01:15:20 +0530

    first commit

---
+++ src/main/java/com/spring/ai/rag/spring_ai_rag_app/SpringAiRagAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ai.rag.spring_ai_rag_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRagAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiRagAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/ai/rag/spring_ai_rag_app/SpringAiRagAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai.rag.spring_ai_rag_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
